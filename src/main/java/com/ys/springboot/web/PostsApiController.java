3553d699f9616af69e3ec96e70fd87133fa6ff6c
YoungSSS <young4127s@naver.com>
2021-03-14 16:23:12 +0900

    등록 API

--- src/main/java/com/ys/springboot/web/PostsApiController.java
+++ src/main/java/com/ys/springboot/web/PostsApiController.java
@@ -1,4 +1,22 @@
 package com.ys.springboot.web;
 
+import com.ys.springboot.service.posts.PostsService;
+import com.ys.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
 }
