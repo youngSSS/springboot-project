8866edbd4cc99b90b30664a118d2dee9ff03fc77
YoungSSS <young4127s@naver.com>
2021-03-15 21:53:27 +0900

    posts 등록, 수정, 조회

--- src/main/java/com/ys/springboot/service/posts/PostsService.java
+++ src/main/java/com/ys/springboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.ys.springboot.service.posts;
 
+import com.ys.springboot.domain.posts.Posts;
 import com.ys.springboot.domain.posts.PostsRepository;
 
+import com.ys.springboot.web.dto.PostsResponseDto;
 import com.ys.springboot.web.dto.PostsSaveRequestDto;
+import com.ys.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +23,16 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
