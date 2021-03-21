c3cfb18fc0ece06e361a7d24a1b3213080ca282c
YoungSSS <young4127s@naver.com>
2021-03-21 17:45:17 +0900

    Naver login

--- src/main/java/com/ys/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/ys/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,11 +27,32 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        // Naver에서는 reponse 내부에 데이터가 들어있는 상태로 반환되기 때문에 response를 따로 받아서 사용한다
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
