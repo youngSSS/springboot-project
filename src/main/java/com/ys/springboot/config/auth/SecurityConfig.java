3f39029beaf63d4a4191879cd229d4bb946a18cf
YoungSSS <young4127s@naver.com>
2021-05-04 21:38:44 +0900

    CORS: Change addCorsMappings

--- src/main/java/com/ys/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/ys/springboot/config/auth/SecurityConfig.java
@@ -24,9 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+//        http
                 // h2-console 화면을 사용하기 위해 해당 옵션들을 disable 한다
-                .csrf().disable().headers().frameOptions().disable()
+//                .csrf().disable().headers().frameOptions().disable()
 //                .and()
 //                    // authorizeRequests로 URL별 권한 관리를 설정한다
 //                    // authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있다
@@ -38,19 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 ////                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
 //                    .antMatchers("/api/v1/**").permitAll()
 //                    .anyRequest().authenticated()
-                .and()
-                    // Logout 기능 설정의 진입점
-                    .logout()
-                        // Logout 성공 시 / 주소로 이동
-                        .logoutSuccessUrl("/")
-                .and()
-                    // OAuth 2 Login 기능 설정의 진입점
-                    .oauth2Login()
-                        // OAuth 2 Login 성공 이후 사용자 정보를 가져올 때의 설정 담당
-                        .userInfoEndpoint()
-                            // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
-                            // 리소스 서버 (소셜 서비스들)에서 사용자 정보를 가젼온 상태에서 추가로 진행하고자 하는 기능을 명시할 수 있다
-                            .userService(customOAuth2UserService);
+//                .and()
+//                    // Logout 기능 설정의 진입점
+//                    .logout()
+//                        // Logout 성공 시 / 주소로 이동
+//                        .logoutSuccessUrl("/")
+//                .and()
+//                    // OAuth 2 Login 기능 설정의 진입점
+//                    .oauth2Login()
+//                        // OAuth 2 Login 성공 이후 사용자 정보를 가져올 때의 설정 담당
+//                        .userInfoEndpoint()
+//                            // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+//                            // 리소스 서버 (소셜 서비스들)에서 사용자 정보를 가젼온 상태에서 추가로 진행하고자 하는 기능을 명시할 수 있다
+//                            .userService(customOAuth2UserService);
     }
 
     private AuthenticationService authenticationService;
